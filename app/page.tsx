696279cf632a210bc57eae853972604c1fec69a9
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-09 16:54:31 +0530

    removed card from landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import { BuyCard } from "@/components/Home/buyCard";
 import Landing from "@/components/Home/Landing";
 
 export default function Home() {
   return (
     <>
-    <Landing/>
-    <main className="flex min-h-screen flex-col items-center px-10 pt-10 pb-8">
-    <div>
-      <BuyCard 
-      productName="Wireless Headphones" 
-      productPrice="$99" 
-      productRating={3} 
-      productDescription="High-quality audio with noise cancellation" />
-    </div>
-    </main>
+      <main className="flex min-h-screen flex-col items-center px-10 pt-10 pb-8">
+        <Landing />
+      </main>
     </>
   );
 }
