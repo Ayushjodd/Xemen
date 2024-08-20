628bca9b3a5d2e12df881e584ef1d8a040df845e
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-20 13:55:11 +0530

    get product by the id route added

---
+++ app/api/product/[id]/route.ts
@@ -0,0 +1,64 @@
+"use server"
+
+import { authOptions } from "@/app/lib/auth";
+import prisma from "@/db/db";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET({ params }: { params: { id: string } }) {
+    try {
+        const session = await getServerSession(authOptions);
+
+        if (!session) {
+            return NextResponse.json({
+                success: false, message: "Unauthorized"
+            }, { status: 401 });
+        }
+
+        const { id } = params; 
+
+        const product = await prisma.product.findUnique({
+           where:{
+            id:id
+           },
+           select:{
+            id:true,
+            title:true,
+            description:true,
+            price:true,
+            imageUrl:true,
+            category:true,
+            sellerName:true,
+            sellerId:true,
+            createdAt:true,
+            updatedAt:true
+           }
+        })
+
+     if(!product){
+        return NextResponse.json({success:false,message:"Product dosent exists!"},{status:404})
+     }
+
+
+        return NextResponse.json({
+           id:product.id,
+           title:product.title,
+           description:product.description,
+           price:(Number(product.price) / Number(BigInt(LAMPORTS_PER_SOL))).toFixed(2),
+           imageUrl:product.imageUrl,
+           category:product.category,
+           sellerName:product.sellerName,
+           sellerId:product.sellerId,
+           createdAt:product.createdAt,
+           updatedAt:product.updatedAt
+        }, { status: 200 });
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { success: false, message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
