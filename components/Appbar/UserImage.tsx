e2ca71caf2aee3ca8cc41f248c8fef1a0452bfcf
rushhaaland <rudrasankhasinhamahapatra@gmail.com>
2024-08-28 17:05:47 +0530

    fixing

--- components/Appbar/UserImage.tsx
+++ components/Appbar/UserImage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Image from 'next/image'
-export default function UserImage({ image }: any) {
+import Image from 'next/image';
+
+export default function UserImage({ image }: { image: string }) {
     return (
-        <div>
+        <div className="w-8 h-8">
             <Image
                 className="w-full h-full rounded-full cursor-pointer"
                 src={image || ""}
-                width={100}
-                height={100}
+                width={32}
+                height={32}
                 alt="user_profile_image"
                 referrerPolicy="no-referrer"
             />
