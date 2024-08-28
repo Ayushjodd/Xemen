22d2300f245c4e6961b60d8fe981c4c8c30985dd
rushhaaland <rudrasankhasinhamahapatra@gmail.com>
2024-08-28 15:53:17 +0530

    push

--- components/Home/ALLitems.tsx
+++ components/Home/ALLitems.tsx
@@ -94,7 +94,7 @@ export default function AllItems() {
                   key={product.id}
                   url={product.imageUrl}
                   title={product.title}
-                  description={product.description}
+                  description={product.description.slice(0, 30) + "..."}
                   price={product.price}
                 />
               ))}
