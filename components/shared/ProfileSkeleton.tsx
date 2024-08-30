479f2fb5429a4c528e843316cf41b3e043714bf4
rushhaaland <rudrasankhasinhamahapatra@gmail.com>
2024-08-31 03:33:33 +0530

    removing comments

--- components/shared/ProfileSkeleton.tsx
+++ components/shared/ProfileSkeleton.tsx
@@ -7,16 +7,16 @@ export const ProfileSkeleton = () => {
 
                     <div className="flex flex-col items-center justify-center mb-7 overflow-hidden h-screen">
                         <div className="flex flex-col items-center">
-                            <div className="w-48 h-6 bg-gray-200 rounded-lg dark:bg-gray-700 mb-4"></div> {/* Profile Heading Skeleton */}
+                            <div className="w-48 h-6 bg-gray-200 rounded-lg dark:bg-gray-700 mb-4"></div> 
                         </div>
                         <div className="flex flex-col border bg-gray-50 shadow-lg rounded-lg px-6 py-5 w-[60%] my-4 gap-5 relative">
-                            <div className="w-full h-6 bg-gray-200 rounded-lg dark:bg-gray-700 mb-4"></div> {/* Name Skeleton */}
-                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> {/* Email Skeleton */}
-                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> {/* Name Skeleton */}
-                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> {/* Provider Skeleton */}
-                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> {/* Created At Skeleton */}
-                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> {/* Orders Created Skeleton */}
-                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> {/* Products Listed Skeleton */}
+                            <div className="w-full h-6 bg-gray-200 rounded-lg dark:bg-gray-700 mb-4"></div> 
+                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> 
+                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> 
+                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> 
+                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> 
+                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> 
+                            <div className="w-full h-4 bg-gray-200 rounded-lg dark:bg-gray-700 mb-2"></div> 
                         </div>
                     </div>
                 </div>
