617ea4a972bdb0ee94db665791e87817f41da130
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-30 18:30:06 +0530

    removed router

--- components/Home/Landing.tsx
+++ components/Home/Landing.tsx
@@ -1,7 +1,4 @@
-
 "use client";
-
-import { useRouter } from "next/navigation";
 import Appbar from "../Appbar/Appbar";
 import { Footer } from "../shared/Footer";
 import { Working } from "./Working";
@@ -11,29 +8,27 @@ import { Listings } from "./Listings";
 import { Main } from "./Main";
 
 export default function Component() {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <Appbar />
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-          <Main/>
+          <Main />
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
-          <Listings/>
+          <Listings />
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32">
-        <Features/>
+          <Features />
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
-        <Benifits/>
+          <Benifits />
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32">
-        <Working/>
+          <Working />
         </section>
       </main>
-   <Footer/>
+      <Footer />
     </div>
   );
 }
