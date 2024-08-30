f8e0e6fd18a8684f990ecbfce7ed5becfc75b41a
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-30 18:30:44 +0530

    updated

--- components/Appbar/UserImage.tsx
+++ components/Appbar/UserImage.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function UserImage({ image }: { image: string }) {
-    return (
-        <div className="w-8 h-8">
-            <Image
-                className="w-full h-full rounded-full cursor-pointer"
-                src={image || ""}
-                width={32}
-                height={32}
-                alt="user_profile_image"
-                referrerPolicy="no-referrer"
-            />
-        </div>
-    );
+  return (
+    <div className="w-9 h-9  rounded-full ">
+      <Image
+        className="w-full h-full rounded-full cursor-pointer"
+        src={image || ""}
+        width={32}
+        height={32}
+        alt="user_profile_image"
+        referrerPolicy="no-referrer"
+      />
+    </div>
+  );
 }
