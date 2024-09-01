543ee64abd4cd36ecdc80cd1fd6c7c45c19f2824
rushhaaland <rudrasankhasinhamahapatra@gmail.com>
2024-09-01 22:36:33 +0530

    Little improvements

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { authOptions } from '@/app/lib/auth'
 import NextAuth from 'next-auth/next'
 
+export const maxDuration =60;
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
