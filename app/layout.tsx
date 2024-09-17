2d71f73cdb2212f5ca11152f1c5bd807fac2bb35
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-17 13:30:50 +0530

    added next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import TopLoader from "@/components/shared/TopLoader";
+import { ThemeProvider } from "@/components/Home/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Xemen",
   description:
-    "Xemen is a web3 solana ecommerce site where people can come and buy products by solana or they can list a product for selling in solana.Xemen also provides a wallet for their users .Xemen supports solana adding from solana and real time solana transaction from buyers to sellers.It is built with next js,tailwind css,react js",
+    "Xemen is a web3 solana ecommerce site where people can come and buy products by solana or they can list a product for selling in solana. Xemen also provides a wallet for their users. Xemen supports solana adding from solana and real-time solana transactions from buyers to sellers. It is built with Next.js, Tailwind CSS, React.js.",
 };
 
 export default function RootLayout({
@@ -21,7 +22,16 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <TopLoader />
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </Providers>
     </html>
   );
