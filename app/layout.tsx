cc8bf1431cb29329bfa76fed6b41a75aaac88cc9
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-17 12:54:54 +0530

    added toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import TopLoader from "@/components/shared/TopLoader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Xemen",
-  description: "Xemen is a web3 solana ecommerce site where people can come and buy products by solana or they can list a product for selling in solana.Xemen also provides a wallet for their users .Xemen supports solana adding from solana and real time solana transaction from buyers to sellers.It is built with next js,tailwind css,react js",
+  description:
+    "Xemen is a web3 solana ecommerce site where people can come and buy products by solana or they can list a product for selling in solana.Xemen also provides a wallet for their users .Xemen supports solana adding from solana and real time solana transaction from buyers to sellers.It is built with next js,tailwind css,react js",
 };
 
 export default function RootLayout({
@@ -18,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-      <body className={inter.className}>{children}</body>
+        <TopLoader />
+        <body className={inter.className}>{children}</body>
       </Providers>
     </html>
   );
